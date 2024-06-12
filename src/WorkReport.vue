103c10145991d9fe878b655d5ad91b2e107db89a
Jac Wang <lu7766lu7766@gmail.com>
2024-06-12 09:43:28 +0800

    優化

--- src/WorkReport.vue
+++ src/WorkReport.vue
@@ -1,28 +1,28 @@
 <template>
-  <div>
+  <div class="p-4">
     <div>
-      Display Type
-      <div>
-        <label>
-          Personal
+      查詢方式
+      <div class="mt-1">
+        <label class="mr-3">
+          <span class="mr-1"> 依姓名 </span>
           <input type="radio" :value="DispalyType.Personal" v-model="displayType" />
         </label>
         <label>
-          Dist
+          <span class="mr-1"> 依所屬區 </span>
           <input type="radio" :value="DispalyType.Dist" v-model="displayType" />
         </label>
       </div>
     </div>
-    <div v-if="displayType == DispalyType.Personal">
-      Name: <input type="text" v-model="searchName" class="border" />
-      <table class="border" v-if="personalData">
+    <div v-if="displayType == DispalyType.Personal" class="mt-4">
+      輸入欲查詢姓名: <input type="text" v-model="searchName" class="border rounded w-44 px-2 py-1" placeholder="須與表單填入相符" />
+      <table class="mt-1">
         <thead>
           <tr>
             <th class="border whitespace-nowrap px-3 py-1" v-for="title in personalTitles">{{ title }}</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
+          <tr v-if="personalData">
             <td class="border text-center" v-for="title in personalTitles" :style="personalData[title] == personalData.max ? 'color: red' : ''">
               {{ personalData[title] }}
             </td>
@@ -30,19 +30,19 @@
         </tbody>
       </table>
     </div>
-    <div v-if="displayType == DispalyType.Dist">
-      <select v-model="selectedDist">
+    <div v-if="displayType == DispalyType.Dist" class="mt-4">
+      <select v-model="selectedDist" class="w-40 px-2 py-1 border rounded">
         <option value="">所屬區</option>
         <option v-for="distName in distOptions" :value="distName">{{ distName }}</option>
       </select>
-      <table>
+      <table class="mt-1">
         <thead>
           <tr>
             <th class="border whitespace-nowrap px-3 py-1" v-for="title in titles">{{ title }}</th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="datas in groupByAreaDatas[selectedDist]">
+          <tr v-for="datas in groupByDistDatas[selectedDist]">
             <td class="text-center border" v-for="(title, index) in titles" :style="datas[title] == datas.max ? 'color: red' : ''">
               {{ datas[title] }}
             </td>
@@ -73,7 +73,11 @@ const groupIndex = [
   { name: "社交型", start: 43, end: 53 },
   { name: "常規型", start: 53, end: 63 },
 ]
-// 使用 fetch 打 API
+const NAME_FIELD_TITLE = "你的名字"
+const DIST_FIELD_TITLE = "你所屬的區/組織"
+const LIKE_ANSWER = "喜歡"
+
+// data proccess
 const sourceDatas = ref<string[][]>([[]])
 const basicLength = ref(0)
 const titles = computed(() => sourceDatas.value[0].concat(groupIndex.map((x) => x.name)))
@@ -83,43 +87,52 @@ const arrayDatas = computed(() =>
       groupIndex.map((groupInfo) =>
         datas
           .slice(groupInfo.start, groupInfo.end)
-          .filter((y) => y == "喜歡")
+          .filter((answer) => answer == LIKE_ANSWER)
           .length.toString()
       )
     )
   )
 )
-const datas = computed<any[]>(() =>
+const datas = computed(() =>
   arrayDatas.value.map((values) => {
     return values.reduce((res, value, index) => Object.assign(res, { [titles.value[index]]: value }), {
       max: Math.max(...values.slice(basicLength.value).map((x) => +x)),
-    })
+    }) as Record<string, string | number>
   })
 )
-const groupByAreaDatas = computed<any>(() => _.groupBy(datas.value, "你所屬的區/組織"))
+const groupByDistDatas = computed(() => _.groupBy(datas.value, DIST_FIELD_TITLE))
+
+// global
 enum DispalyType {
   Personal,
   Dist,
 }
-
 const displayType = ref<DispalyType>(DispalyType.Personal)
+
+// personal
 const searchName = ref("")
-const personalData = computed(() => (searchName.value ? datas.value.find((data) => data["你的名字"] == searchName.value) : null))
+const personalData = computed(() => (searchName.value ? datas.value.find((data) => data[NAME_FIELD_TITLE] == searchName.value) : null))
 const personalTitles = computed(() => titles.value.slice(0, 3).concat(titles.value.slice(basicLength.value)))
 
-const distOptions = computed(() => Object.keys(groupByAreaDatas.value))
+// dist
+const distOptions = computed(() => Object.keys(groupByDistDatas.value))
 const selectedDist = ref("")
 
+// fetch
 const fetchData = () => {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      data.values.push(mock)
+      if (data.values.length <= 1) {
+        data.values.push(mock)
+      }
       sourceDatas.value = data.values
-      console.log(groupByAreaDatas.value)
       basicLength.value = data.values[0].length
     })
-    .catch((error) => console.error("Error:", error))
+    .catch((error) => {
+      alert(`讀取失敗，請從新整理或聯繫管理員。\n錯誤訊息：${error?.message}`)
+      console.error("Error:", error)
+    })
 }
 
 onMounted(() => {
